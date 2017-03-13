45c457c5046e8826a1974d42c6f52507a124f2f4
Małgorzata Sobczak <sobczakmalgorz@gmail.com>
2017-03-13 10:41:06 +0100

    Adding Tests and simple testing bitmap

---
+++ ImageRecognition.ObjectDetectionLibrary.Tests/GrayScaleMatrixTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Drawing;
+using Xunit;
+
+namespace ImageRecognition.ObjectDetectionLibrary.Tests
+{
+    public class GrayScaleMatrixTests
+    {
+        [Fact]
+        public void GSTypeTest()
+        {
+            Bitmap b = (Bitmap)Image.FromFile(@"F:\OneDrive\Dokumenty\Visual Studio 2017\Projects\ImageRecognition\TestBitmaps\bitmap_01.bmp", true);
+            var m = new GrayScaleMatrix(b);
+            Assert.IsType<GrayScaleMatrix>(m);
+        }
+
+        [Fact]
+        public void GSTest()
+        {
+            IGrayScaleMatrix m;
+            Bitmap b = (Bitmap)Image.FromFile(@"F:\OneDrive\Dokumenty\Visual Studio 2017\Projects\ImageRecognition\TestBitmaps\bitmap_01.bmp", true);
+            m = new GrayScaleMatrix(b);
+            //Bitmap temp = m.GetBitmap;
+            Assert.Equal(m.imageMatrix[3, 0], 255);
+        }
+
+        //[Fact]
+        //public void GSBitmapReturnTypeTest()
+        //{
+        //    IGrayScaleMatrix m;
+        //    Bitmap b = (Bitmap)Image.FromFile(@"F:\OneDrive\Dokumenty\Visual Studio 2017\Projects\ImageRecognition\TestBitmaps\bitmap_01.bmp", true);
+        //    m = new GrayScaleMatrix(b);
+        //    var temp = m.GetBitmap();
+        //    Assert.IsType<Bitmap>(temp);
+        //}
+
+        //[Fact]
+        //public void GSBitmapReturnTest()
+        //{
+        //    IGrayScaleMatrix m;
+        //    Bitmap b = (Bitmap)Image.FromFile(@"F:\OneDrive\Dokumenty\Visual Studio 2017\Projects\ImageRecognition\TestBitmaps\bitmap_01.bmp", true);
+        //    m = new GrayScaleMatrix(b);
+        //    var temp = m.GetBitmap();
+        //    Assert.Equal((int)temp.GetPixel(3, 3).R, (int)b.GetPixel(3, 3).R);
+        //}
+    }
+}
---
+++ ImageRecognition.ObjectDetectionLibrary.Tests/ImageCanalMatrixTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using Xunit;
+
+namespace ImageRecognition.ObjectDetectionLibrary.Tests
+{
+    public class ImageCanalMatrixTests
+    {
+        [Fact]
+        public void TypeTest()
+        {
+            IImageCanalMatrix m;
+            m = new ImageCanalMatrix(new int[2,2]{ {0,0 },{0,0 } });
+            Assert.IsType<ImageCanalMatrix>(m);
+        }
+
+        [Fact]
+        public void WidthTest()
+        {
+            IImageCanalMatrix m;
+            m = new ImageCanalMatrix(new int[4, 2] { { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 } });
+            Assert.Equal(m.Width, 4);
+        }
+
+        [Fact]
+        public void HeightTest()
+        {
+            IImageCanalMatrix m;
+            m = new ImageCanalMatrix(new int[4, 2] { { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 } });
+            Assert.Equal(m.Height, 2);
+        }
+
+        [Fact]
+        public void matrixSetTest()
+        {
+            IImageCanalMatrix m;
+            m = new ImageCanalMatrix(new int[4, 2] { { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 } });
+            m.imageMatrix[2, 1] = 9;
+            Assert.Equal(m.imageMatrix[2, 1], 9);
+        }
+
+        [Fact]
+        public void EmptyCreatorTest()
+        {
+            IImageCanalMatrix m;
+            m = new ImageCanalMatrix();
+            Assert.Equal(m.imageMatrix[0, 0], -1);
+        }
+
+        [Fact]
+        public void EmptyCreatorWidthTest()
+        {
+            IImageCanalMatrix m;
+            m = new ImageCanalMatrix();
+            Assert.Equal(m.Width, 0);
+        }
+
+        [Fact]
+        public void EmptyCreatorHeightTest()
+        {
+            IImageCanalMatrix m;
+            m = new ImageCanalMatrix();
+            Assert.Equal(m.Height, 0);
+        }
+
+        
+    }
+}
