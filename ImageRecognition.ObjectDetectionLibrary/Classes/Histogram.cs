c9381ba61b6d036924be4be0e51cb026e14162ab
Małgorzata Sobczak <sobczakmalgorz@gmail.com>
2017-03-22 14:27:37 +0100

    Adding histograms classes (for further improvement)

---
+++ ImageRecognition.ObjectDetectionLibrary/Classes/Histogram.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageRecognition.ObjectDetectionLibrary.Classes
+{
+    class Histogram
+    {
+        public int[] histogram { get; set; }
+
+        public Histogram()
+        {
+            histogram = new int[256];
+            for (int i = 0; i < 256; i++)
+            {
+                histogram[i] = 0;
+            }
+        }
+
+        public Histogram(int[,] m)
+        {
+            histogram = new int[256];
+            for (int i = 0; i < 256; i++)
+            {
+                histogram[i] = 0;
+            }
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    histogram[m[i, j]]++;
+                }
+            }
+        }
+    }
+}
---
+++ ImageRecognition.ObjectDetectionLibrary/Classes/RGBHistogram.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageRecognition.ObjectDetectionLibrary.Classes
+{
+    class RGBHistogram : Histogram
+    {
+        public RGBHistogram(Bitmap m)
+        {
+            histogram = new int[256];
+            for (int i = 0; i < 256; i++)
+            {
+                histogram[i] = 0;
+            }
+            for (int i = 0; i < m.Width; i++)
+            {
+                for (int j = 0; j < m.Height; j++)
+                {
+                    Color pixel = m.GetPixel(i, j);
+                    int p = pixel.ToArgb();
+
+                    if (pixel != null)
+                    {
+                        //picture data
+                        histogram[pixel.R]++;
+                        histogram[pixel.G]++;
+                        histogram[pixel.B]++;
+                    }
+                    
+                }
+            }
+        }
+    }
+}
